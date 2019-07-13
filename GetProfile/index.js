83504bfef40f4b7b2a588e894af6a22e0e5df9d9
George Ziady <george@springfisher.com>
2019-07-14 00:52:40 +0200

    Calculate scores over set intervals.

--- GetProfile/index.js
+++ GetProfile/index.js
@@ -25,6 +25,23 @@ module.exports = function (context, req) {
       const points = response.body.entities.user[profileName].points;
       const challenges = response.body.entities.user[profileName].completedChallenges;
 
+      // The list of intervals we're interested in recording.
+      let intervals = [1,7,30,90,180,365];
+
+      const pointsTable = intervals.reduce((tally, days) => {
+        tally[days] = challenges.reduce((total, challenge) => {
+          // Calculate the date X days ago for comparison.
+          let date = new Date();
+          let daysAgo = date.setDate(date.getDate() - days);
+
+          if (challenge.completedDate > daysAgo) {
+            total ++;
+          }
+          return total;
+        }, 0);
+        return tally;
+      } , {})
+
       let isValid = true;
       if (response.body.entities.user[profileName].isCheater
           || !response.body.entities.user[profileName].isHonest) {
@@ -36,12 +53,21 @@ module.exports = function (context, req) {
         {
           'username': profileName,
           'points' : points,
+          'pointsTable' : pointsTable,
           'challenges' : challenges,
           'valid' : isValid
         }
 
       let output = `FreeCodeCamp profile for ${profileName} \n`;
       output += `points: ${userSave.points} \n`;
+      // Include a list of the users scores across the intervals.
+      output += "pointsTable: \n" + Object.keys(userSave.pointsTable).map(function(key, index) {
+        if (key == 1) {
+          return `  in last 24 hours: ${userSave.pointsTable[key]} \n`
+        } else {
+          return `  in last ${key} days: ${userSave.pointsTable[key]} \n`
+        }
+      }).join('');
 
       // TODO: persist to DB.
 
