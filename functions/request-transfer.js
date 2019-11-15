3e6ed22afea650b776a921cec960309b5c7d0f4b
George Ziady <george@springfisher.com>
2019-11-15 14:28:30 +0200

    Send SMS when request for transfer received.

--- functions/request-transfer.js
+++ functions/request-transfer.js
@@ -1,7 +1,24 @@
 'use strict';
 const AWS = require('aws-sdk');
+const twilio = require('twilio');
 
 module.exports.handler = (event, context, callback) => {
+  var accountSid = process.env.twilio_sid;
+  var authToken = process.env.twilio_auth_token;
+
+  const sourcePhone = process.env.twilio_phone;
+  const targetPhone = 'INSERT-PHONE-HERE';
+
+  var twilio = require('twilio');
+  var client = new twilio(accountSid, authToken);
+
+  client.messages.create({
+      body: 'Transfer requested for Miriam.',
+      to: targetPhone,  // Text this number
+      from: sourcePhone // From a valid Twilio number
+  })
+  .then((message) => console.log(message.sid));
+
   const response = {
     statusCode: 200,
     body: JSON.stringify({
