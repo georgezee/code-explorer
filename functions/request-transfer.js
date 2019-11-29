93ad5693d70b600d541f71c6ad559f95677993f8
George Ziady <george@springfisher.com>
2019-11-29 15:33:24 +0200

    Add resources to enable local debugging.

--- functions/request-transfer.js
+++ functions/request-transfer.js
@@ -24,6 +24,13 @@ module.exports.handler = (event, context, callback) => {
 
   const response = {
     statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT",
+      "Access-Control-Allow-Headers": "*",
+      "Access-Control-Allow-Credentials": "true"
+    },
     body: JSON.stringify({
       message: 'Message sent'
     }),
